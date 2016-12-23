f343ff4992ea29964cf950f1833e0b1474e3859b
David Megginson <david.megginson@megginson.com>
2016-12-23 16:50:45 -0500

    Preserve nodes as well as ways and relations, so that we get populated-places points.

--- scripts/make-layers.sh
+++ scripts/make-layers.sh
@@ -8,7 +8,7 @@ LAYERS="natural waterway highway railway landuse place boundary"
 
 for LAYER in $LAYERS; do
     echo Extracting $LAYER ...
-    osmfilter "tiles/$INPUT" --keep= --keep-ways="$LAYER=" --keep-relations="$LAYER=" -o=tmp/$LAYER.osm
+    osmfilter "tiles/$INPUT" --keep= --keep-nodes="$LAYER=" --keep-ways="$LAYER=" --keep-relations="$LAYER=" -o=tmp/$LAYER.osm
     echo Converting $LAYER to Shapefile ...
     rm -rf shapefiles/$LAYER
     ogr2ogr --config OSM_CONFIG_FILE config/osmconf.ini -skipfailures -f 'ESRI Shapefile' shapefiles/$LAYER tmp/$LAYER.osm
